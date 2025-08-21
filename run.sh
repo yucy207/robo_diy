aed98ee89d59b98a8e1d79d29d83736d70890534
yucy207 <chenyangyu.207@gmail.com>
2025-08-21 14:57:46 +0800

    mix motor

--- run.sh
+++ run.sh
@@ -2,3 +2,7 @@ huggingface-cli download --repo-type dataset  yucy207/robopanoptes_test12 --cach
 
 python process_data_from_lerobot.py 
 python train.py --config-name=train_diffusion_transformer_snake_workspace task.dataset_path=./dataset.zarr.zip 
+
+dataset_id="_0818"
+cache_dir="/data/yuchenyang/snake_traj"
+huggingface-cli download --repo-type dataset  yucy207/robopanoptes_test${dataset_id} --cache-dir ${cache_dir}
